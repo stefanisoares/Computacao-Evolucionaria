a4a0027cded4c9f56efcf348b86103774c2b47dc
AugustoPAraujo <49997858+AugustoPAraujo@users.noreply.github.com>
2024-10-25 16:33:43 -0300

    funções para valor e fitness

--- atividades-semanais/atividade3/atv3.py
+++ atividades-semanais/atividade3/atv3.py
@@ -1,28 +1,8 @@
 import random
 import copy
-import numpy as np
 import matplotlib.pyplot as plt
 
-
-def selecao_roleta(pop, fitness):
-    soma_fitness = sum(fitness)
-    probabilidade = []
-    cumulativa = 0
-    list_cumulativa = []
-    list_cumulativa.clear()
-    for i in fitness:
-        probabilidade.append(i/soma_fitness)
-        cumulativa = cumulativa + i/soma_fitness
-        list_cumulativa.append(cumulativa)
-    #print(probabilidade)
-
-    r = random.random()
-    for i, p in enumerate(list_cumulativa):
-        if r < p:
-            return pop[i]
-
-
-# Dados iniciais
+# Dados iniciais ###############################################################################
 
 data = {
     "Item": list(range(1, 23)),
@@ -39,9 +19,9 @@ data = {
 itens = 22
 pop = []
 mochila = []
-n_pop = 100
+n_pop = 10
 geracao = []
-fitness = []
+fitness_pop = []
 fitness_mut = []
 fitness_cross = []
 valor_pop = []
@@ -49,6 +29,7 @@ filho1 = []
 filho2 = []
 filhos = []
 pais = []
+taxa_mutacao = 0.03
 mem_valor = 0
 count_patamar = 0
 num_patamar = 10
@@ -56,39 +37,86 @@ ite = 0
 num_ite = 1000
 graf = {"Valor": [], "Fitness": []}
 
-# Populacao inicial ###############################################################################
+# Funções ###############################################################################
 
+def selecao_roleta(pop, fitness):
+    soma_fitness = sum(fitness)
+    probabilidade = []
+    cumulativa = 0
+    list_cumulativa = []
+    list_cumulativa.clear()
+    for i in fitness:
+        probabilidade.append(i/soma_fitness)
+        cumulativa = cumulativa + i/soma_fitness
+        list_cumulativa.append(cumulativa)
+    # print(probabilidade)
+    # print(list_cumulativa)
+
+    r = random.random()
+    for i, p in enumerate(list_cumulativa):
+        if r < p:
+            # print(i, p)
+            return pop[i]
+
+def fitness_function(fit):
+    peso = 0
+    for j in range(itens):
+        peso += fit[i][j]*data["Peso (g)"][j]
+        if peso > 3000:
+            peso = 0
+    return peso
+
+def valor_function(vlr):
+    valor = 0
+    for j in range(itens):
+        valor += vlr[i][j]*data["Valor"][j]
+    return valor
+
+
+
+# Populacao inicial ###############################################################################
 
 for i in range(n_pop):
     mochila.clear()
+    mochila = []
     for i in range(itens):
         indice = random.randint(0, 1)
         mochila.append(indice)
     pop.append(mochila.copy())
     
+    print(mochila)
+
+# Valor População inicial ########################################################################
+
+valor_pop.clear()
+
+for i in range(n_pop):
+    valor_pop.append(valor_function(pop))
+    
+print("Valor populacao inicial: ", sum(valor_pop))
+
+
 
 while True:
 
-    # Fitness Function ###############################################################################
-    fitness.clear()  # Limpa a lista de fitness antes de calcular novamente
-    for i in range(n_pop):
-        peso = 0
-        for j in range(itens):
-            peso += pop[i][j]*data["Peso (g)"][j]
-            if peso > 3000:
-                peso = 0
-        fitness.append(peso)
-        
+    # Fitness População ###############################################################################
 
-    #print("Fitness Population: ", sum(fitness))
+    fitness_pop.clear()  # Limpa a lista de fitness antes de calcular novamente
+
+    for i in range(len(pop)):
+        fitness_pop.append(fitness_function(pop))
+
+    print("Fitness População: ", sum(fitness_pop))
+
 
     # Seleção natural ###############################################################################
 
     num_pais = int(n_pop/2)  
     pais.clear()
     for i in range(num_pais):
-        pai = selecao_roleta(pop, fitness)
+        pai = selecao_roleta(pop, fitness_pop)
         pais.append(pai)
+        # print(pai)
 
 
     # Cross Over ###############################################################################
@@ -116,14 +144,10 @@ while True:
     fitness_cross.clear()
 
     for i in range(len(filhos)):
-        peso = 0
-        for j in range(itens):
-            peso = peso + filhos[i][j]*data["Peso (g)"][j]
-            if peso > 3000:
-                peso = 0
-        fitness_cross.append(peso)
+        fitness_cross.append(fitness_function(filhos))
 
-    #print("Fitness Cross Over: ", sum(fitness_cross))
+    print("Fitness Cross Over: ", sum(fitness_cross))
+
 
     # Mutacao ###############################################################################
 
@@ -132,7 +156,7 @@ while True:
     for i in filhos:
         r = random.random()
         filho = copy.deepcopy(i)
-        if r <= 0.05:
+        if r <= taxa_mutacao:
             indice = random.randint(0, itens-1)
             filho[indice] = 1 - filho[indice]  
         geracao.append(filho)
@@ -142,15 +166,11 @@ while True:
 
     fitness_mut.clear()
 
-    for i in range(n_pop):
-        peso = 0
-        for j in range(22):
-            peso = peso + geracao[i][j]*data["Peso (g)"][j]
-            if peso > 3000:
-                peso = 0
-        fitness_mut.append(peso)
-        
-    #print("Fitness Mutacao: ", sum(fitness_mut))
+    for i in range(len(geracao)):
+        fitness_mut.append(fitness_function(geracao))
+
+    print("Fitness Mutacao: ", sum(fitness_mut))
+
 
     # Nova Populacao ###############################################################################
 
@@ -159,32 +179,30 @@ while True:
     valor_pop.clear()
 
     for i in range(n_pop):
-        valor = 0
-        for j in range(itens):
-            valor = valor + pop[i][j]*data["Valor"][j]
-        valor_pop.append(valor)
+        valor_pop.append(valor_function(pop))
         
-    #print("Valor nova populacao: ", sum(valor_pop))
-      
+    print("Valor nova populacao: ", sum(valor_pop))
+        
     graf["Valor"].append(sum(valor_pop))
     graf["Fitness"].append(sum(fitness_mut))
-        
+
+     
     # Criterio de Parada ###############################################################################
 
-    if ((sum(valor_pop) <= (mem_valor)*1.001) and (sum(valor_pop) >= (mem_valor)*0.99)):
+    if ((sum(valor_pop) <= (mem_valor)*1.01) and (sum(valor_pop) >= (mem_valor)*0.99)):
         count_patamar += 1
     else:
         mem_valor = sum(valor_pop)
     if ((ite >= num_ite)):
     #if ((count_patamar>num_patamar) or (ite >= num_ite)):
+        #print(count_patamar)
         count_patamar = 0
-        print(ite)
+        #print(ite)
         ite = 0
         break
-    
     ite += 1
-    
-    
+
+
 # Plotando o gráfico
 plt.plot(graf["Fitness"])  # 'o' para marcar os pontos
 plt.title("Gráfico de Fitness")  # Título do gráfico
@@ -205,4 +223,3 @@ print("Melhor Solucao: \n")
 for i in range(n_pop):
     print(f"Filho {i}:")
     print([pop[i][j] * data["Item"][j] for j in range(itens)])
-
